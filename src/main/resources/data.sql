739a99596e64eaab1423e63e8dd94b8187ba75c8
Илюша <79216023478@yandex.ru>
2022-07-10 15:45:34 +0300

    style: отредактировать sql-скрипты

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,47 +1,14 @@
-DELETE FROM FRIENDSHIP;
-DELETE FROM USER_LIKE;
-DELETE FROM FILMORATE_USER;
-DELETE FROM film_genres;
-DELETE FROM FILM;
-DELETE FROM GENRE;
-DELETE FROM MPA;
-
-ALTER TABLE FRIENDSHIP ALTER COLUMN id RESTART WITH 1;
-ALTER TABLE FILMORATE_USER ALTER COLUMN id RESTART WITH 1;
-ALTER TABLE FILM ALTER COLUMN id RESTART WITH 1;
-ALTER TABLE GENRE ALTER COLUMN id RESTART WITH 1;
-ALTER TABLE MPA ALTER COLUMN id RESTART WITH 1;
-
 MERGE INTO genre (id, name)
-    VALUES(1, 'Комедия');
-
-MERGE INTO genre (id, name)
-    VALUES(2, 'Драма');
-
-MERGE INTO genre (id, name)
-    VALUES(3, 'Мультфильм');
-
-MERGE INTO genre (id, name)
-    VALUES(4, 'Триллер');
-
-MERGE INTO genre (id, name)
-    VALUES(5, 'Документальный');
-
-MERGE INTO genre (id, name)
-    VALUES(6, 'Боевик');
-
+    VALUES (1, 'Комедия'),
+           (2, 'Драма'),
+           (3, 'Мультфильм'),
+           (4, 'Триллер'),
+           (5, 'Документальный'),
+           (6, 'Боевик');
 
 MERGE INTO mpa (id, name)
-    VALUES(1, 'G');
-
-MERGE INTO mpa (id, name)
-    VALUES(2, 'PG');
-
-MERGE INTO mpa (id, name)
-    VALUES(3, 'PG-13');
-
-MERGE INTO mpa (id, name)
-    VALUES(4, 'R');
-
-MERGE INTO mpa (id, name)
-    VALUES(5, 'NC-17');
+    VALUES (1, 'G'),
+           (2, 'PG'),
+           (3, 'PG-13'),
+           (4, 'R'),
+           (5, 'NC-17');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,54 +1,53 @@
-
 CREATE TABLE IF NOT EXISTS filmorate_user (
-                                              id int AUTO_INCREMENT PRIMARY KEY,
-                                              email varchar(100) UNIQUE,
+    id int AUTO_INCREMENT PRIMARY KEY,
+    email varchar(100) UNIQUE,
     login varchar(100) UNIQUE,
     name varchar(100),
     birthday date
-    );
+);
 
 CREATE TABLE IF NOT EXISTS friendship (
-                                          id int AUTO_INCREMENT PRIMARY KEY,
-                                          active_user_id int,
-                                          passive_user_id int,
-                                          is_accepted boolean DEFAULT FALSE,
-                                          FOREIGN KEY (active_user_id) REFERENCES filmorate_user(id),
+    id int AUTO_INCREMENT PRIMARY KEY,
+    active_user_id int,
+    passive_user_id int,
+    is_accepted boolean DEFAULT FALSE,
+    FOREIGN KEY (active_user_id) REFERENCES filmorate_user(id),
     FOREIGN KEY (passive_user_id) REFERENCES filmorate_user(id)
-    );
+);
 
 CREATE TABLE IF NOT EXISTS genre (
-                                     id int AUTO_INCREMENT PRIMARY KEY,
-                                     name varchar(50)
-    );
+    id int AUTO_INCREMENT PRIMARY KEY,
+    name varchar(50)
+);
 
 CREATE TABLE IF NOT EXISTS mpa (
-                                   id int AUTO_INCREMENT PRIMARY KEY,
-                                   name varchar(50)
-    );
+    id int AUTO_INCREMENT PRIMARY KEY,
+    name varchar(50)
+);
 
 CREATE TABLE IF NOT EXISTS film (
-                                    id int AUTO_INCREMENT PRIMARY KEY,
-                                    name varchar(100),
+    id int AUTO_INCREMENT PRIMARY KEY,
+    name varchar(100),
     description text,
     release_date date,
     duration int,
     mpa_id int,
     rate int,
     FOREIGN KEY (mpa_id) REFERENCES mpa(id)
-    );
+);
 
 CREATE TABLE IF NOT EXISTS user_like (
-                                         user_id int,
-                                         film_id int,
-                                         PRIMARY KEY (user_id, film_id),
+    user_id int,
+    film_id int,
+    PRIMARY KEY (user_id, film_id),
     FOREIGN KEY (user_id) REFERENCES filmorate_user(id),
     FOREIGN KEY (film_id) REFERENCES film(id)
-    );
+);
 
 CREATE TABLE IF NOT EXISTS film_genres (
-                                           film_id int,
-                                           genre_id int,
-                                           PRIMARY KEY (film_id, genre_id),
+    film_id int,
+    genre_id int,
+    PRIMARY KEY (film_id, genre_id),
     FOREIGN KEY (film_id) REFERENCES film(id),
     FOREIGN KEY (genre_id) REFERENCES genre(id)
-    )
+);
